a1dd6fb1eb0cee428a7abc5d93a2d8d8d75c299d
Arsen <arsen@localhost.localdomain>
2020-03-06 22:01:45 +0400

    Initial Commit

--- script.js
+++ script.js
@@ -1,36 +1,37 @@
-var btn = document.getElementsByClassName('btn-next');
-for(var i = 0; i < btn.length; i++) {
-    btn[i].addEventListener("click", next)
+var btn = document.getElementsByClassName("btn-next");
+for (var i = 0; i < btn.length; i++) {
+  btn[i].addEventListener("click", next);
 }
 var slide = document.getElementsByClassName("slide");
 var num = 0;
 function next() {
-    num++;
-    if(num == slide.length) {
-        num = 0;
-    }
-    for(var i = 0; i < slide.length; i++) {
-        slide[i].style.display = 'none'
-    }
-    slide[num].style.display = 'flex';
+  num++;
+  if (num == slide.length) {
+    num = 0;
+  }
+  for (var i = 0; i < slide.length; i++) {
+    slide[i].style.display = "none";
+  }
+  slide[num].style.display = "flex";
 }
 
 var modal = document.getElementsByClassName("modal-parent");
 function myFunc() {
-    modal[0].style.display = 'block';
+  modal[0].style.display = "block";
 }
-let exit = document.getElementById('close')
-window.onclick = function (e) {
-    if(e.target == modal[0]) {
-        modal[0].style.display = 'none'
-    }
-}
-exit.addEventListener("click", close)
-document.getElementById("submit").addEventListener("click", close)
+let exit = document.getElementById("close");
+window.onclick = function(e) {
+  if (e.target == modal[0]) {
+    modal[0].style.display = "none";
+  }
+};
+exit.addEventListener("click", close);
+document.getElementById("submit").addEventListener("click", close);
 function close() {
-    modal[0].style.display = 'none';
+  modal[0].style.display = "none";
 }
-for (var i of document.getElementsByClassName('container')) {
-    i.style.maxWidth = screen.availWidth + 'px';
+for (var i of document.getElementsByClassName("container")) {
+  i.style.maxWidth = screen.availWidth + "px";
 }
-document.getElementsByClassName('container')[1].style.maxHeight = screen.availHeight + 'px'
+document.getElementsByClassName("container")[1].style.maxHeight =
+  screen.availHeight + "px";
